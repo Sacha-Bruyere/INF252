b3e45fcdf394b55a3387238cfe3501b798caf42b
Sacha Bruyere <bruyeres@val-etu-drt-w03.u-ga.fr>
2022-01-28 11:57:59 +0100

    Commit 2 | 28/01/2022 11:57

--- copiedir.sh
+++ copiedir.sh
@@ -2,8 +2,11 @@
 cd $HOME/INF252
 mkdir -p TP1
 cd TP1
-cp /partage_etu/Science/INF/123_public/TP1/*
+cp /partage_etu/Science/INF/123_public/TP1/* .
 echo le repertoire courant est
 pwd
 echo il contient
-ls -l
+ls -l 
+
+
+
